1171cc9d6bb648957a4ad21793f824ffbd974827
Pascal Garber <pascal@jumplink.eu>
2016-02-19 10:20:26 -0500

    start with svg stuff

---
+++ assets/apps/android/javascript/users/UserController.js
@@ -0,0 +1,90 @@
+(function(){
+
+  angular
+       .module('users')
+       .controller('UserController', [
+          'userService', '$mdSidenav', '$mdBottomSheet', '$log', '$q',
+          UserController
+       ]);
+
+  /**
+   * Main Controller for the Angular Material Starter App
+   * @param $scope
+   * @param $mdSidenav
+   * @param avatarsService
+   * @constructor
+   */
+  function UserController( userService, $mdSidenav, $mdBottomSheet, $log) {
+    var self = this;
+
+    self.selected     = null;
+    self.users        = [ ];
+    self.selectUser   = selectUser;
+    self.toggleList   = toggleUsersList;
+    self.makeContact  = makeContact;
+
+    // Load all registered users
+
+    userService
+          .loadAllUsers()
+          .then( function( users ) {
+            self.users    = [].concat(users);
+            self.selected = users[0];
+          });
+
+    // *********************************
+    // Internal methods
+    // *********************************
+
+    /**
+     * Hide or Show the 'left' sideNav area
+     */
+    function toggleUsersList() {
+      $mdSidenav('left').toggle();
+    }
+
+    /**
+     * Select the current avatars
+     * @param menuId
+     */
+    function selectUser ( user ) {
+      self.selected = angular.isNumber(user) ? $scope.users[user] : user;
+    }
+
+    /**
+     * Show the Contact view in the bottom sheet
+     */
+    function makeContact(selectedUser) {
+
+        $mdBottomSheet.show({
+          controllerAs  : "cp",
+          templateUrl   : '/themes/jumplink/assets/apps/android/templates/contactSheet.html',
+          controller    : [ '$mdBottomSheet', ContactSheetController],
+          parent        : angular.element(document.getElementById('content'))
+        }).then(function(clickedItem) {
+          $log.debug( clickedItem.name + ' clicked!');
+        });
+
+        /**
+         * User ContactSheet controller
+         */
+        function ContactSheetController( $mdBottomSheet ) {
+          this.user = selectedUser;
+          this.actions = [
+            { name: 'Phone'       , icon: 'phone'       , icon_url: '/themes/jumplink/assets/apps/android/images/phone.svg'},
+            { name: 'Twitter'     , icon: 'twitter'     , icon_url: '/themes/jumplink/assets/apps/android/images/twitter.svg'},
+            { name: 'Google+'     , icon: 'google_plus' , icon_url: '/themes/jumplink/assets/apps/android/images/google_plus.svg'},
+            { name: 'Hangout'     , icon: 'hangouts'    , icon_url: '/themes/jumplink/assets/apps/android/images/hangouts.svg'}
+          ];
+          this.contactUser = function(action) {
+            // The actually contact process has not been implemented...
+            // so just hide the bottomSheet
+
+            $mdBottomSheet.hide(action);
+          };
+        }
+    }
+
+  }
+
+})();
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,14 @@
 $( document ).ready(function() {
     console.log("ready");
-    new Vivus('jumplink_mediamor', {duration: 200}, function() {
-        console.log("Vivus cb");
-    });
+    // new Vivus('jumplink_mediamor', {duration: 200}, function() {
+    //     console.log("Vivus cb");
+    // });
+    var svgElement = document.getElementById("screen");
+    console.log("svg el", svgElement);
+    // var appSvg = Snap(svgElement);
+    // console.log("appSvg", appSvg);
+    // var screen = appSvg.select('#screen');
+    // console.log("screen", screen);
+    
+
 });
