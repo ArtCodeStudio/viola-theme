9cf8ae378d251e90666bc9716f5da97d92d4a891
Pascal Garber <pascal@jumplink.eu>
2016-02-19 19:37:06 -0500

    nice app demo :)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,17 @@
+var movingImageOnMousemove = function () {
+    var movementStrength = 25;
+    var height = movementStrength / $(window).height();
+    var width = movementStrength / $(window).width();
+    $("#apps .svg_apps_col").mousemove(function(e){
+        console.log(e);
+        var pageX = e.pageX - ($(window).width() / 2);
+        var pageY = e.pageY - ($(window).height() / 2);
+        var newvalueX = width * pageX * -1 - 25;
+        var newvalueY = height * pageY * -1 - 50;
+        $('#apps .foreground_finger').css("background-position", newvalueX+"px     "+newvalueY+"px");
+    });
+};
+
 $( document ).ready(function() {
     console.log("ready");
     // new Vivus('jumplink_mediamor', {duration: 200}, function() {
@@ -9,6 +23,6 @@ $( document ).ready(function() {
     // console.log("appSvg", appSvg);
     // var screen = appSvg.select('#screen');
     // console.log("screen", screen);
-    
+    // movingImageOnMousemove();
 
 });
