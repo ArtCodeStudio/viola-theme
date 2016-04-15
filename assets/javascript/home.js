e2253a558d4ac0966c68c20a881e86664eb3ab64
Pascal Garber <pascal@jumplink.eu>
2016-04-15 09:52:29 -0400

    Custom icons and new partial 'services'

--- assets/javascript/home.js
+++ assets/javascript/home.js
@@ -1,19 +1,22 @@
 /**
  * Create Leaflet map
  */
+
+$('#navigation_link').attr("href", window.theme.locationLink)
+
 var map = L.map('map', {
     zoomControl: false,
     attributionControl: true
-}).setView([53.88373, 8.67392], 15);
+}).setView([window.theme.locationLongitude, window.theme.locationLatitude], window.theme.locationZoom);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: 'jumplink.p9j2ob03',
-    accessToken: 'pk.eyJ1IjoianVtcGxpbmsiLCJhIjoiY2lsODd0MGx0MDAyYXdzbHoxNWRzendxZyJ9.6HrdtEXX7n5DhJMNW8PEWA'
+    maxZoom: window.theme.locationMaxZoom,
+    id: window.theme.locationMapboxId,
+    accessToken: window.theme.locationMapboxAccessToken,
 }).addTo(map);
 
-var marker = L.marker([53.88373, 8.67392]).addTo(map);
+var marker = L.marker([window.theme.locationLongitude, window.theme.locationLatitude]).addTo(map);
 
 var changeNavbar = function () {
     var windowScrollTop = $(window).scrollTop();
@@ -21,14 +24,17 @@ var changeNavbar = function () {
     var mainNavbarHeight = 54; //$('#main-navbar').height();
     var actionPosition = viewportHeight - mainNavbarHeight;
     if(windowScrollTop >= actionPosition ) {
-        $('#main-navbar').removeClass('navbar-big');
+        $('#main-navbar, #sidebar').removeClass('navbar-big');
         $('#main-navbar').addClass('bg-white');
     } else {
-        $('#main-navbar').addClass('navbar-big');
-        $('#main-navbar').removeClass('bg-white');
+        $('#main-navbar, #sidebar').addClass('navbar-big');
+        $('#main-navbar, #sidebar').removeClass('bg-white');
     }
 }
 
+/**
+ * Navbar
+ */
 $(window).on('resize scroll', function() {
     changeNavbar();
 });
