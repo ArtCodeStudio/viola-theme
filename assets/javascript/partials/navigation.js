8fb4164f28896692d5a56d6dccaea8f5d21d7c7b
Pascal Garber <pascal@jumplink.eu>
2016-05-08 03:50:19 -0400

    finishing touches

--- assets/javascript/partials/navigation.js
+++ assets/javascript/partials/navigation.js
@@ -5,9 +5,12 @@ var changeNavbar = function () {
     var actionPosition = viewportHeight - mainNavbarHeight;
     if(windowScrollTop >= actionPosition ) {
         $('#main-navbar, #sidebar').removeClass('navbar-big');
+        $('#main-navbar').removeClass('bg-white-transparent');
         $('#main-navbar').addClass('bg-white');
+        
     } else {
         $('#main-navbar, #sidebar').addClass('navbar-big');
+        $('#main-navbar').addClass('bg-white-transparent');
         $('#main-navbar, #sidebar').removeClass('bg-white');
     }
 }
@@ -15,7 +18,7 @@ var changeNavbar = function () {
 /**
  * Navbar
  */
-$(window).on('resize scroll', function() {
+$(window).on('resize scrollstop', function() {
     changeNavbar();
 });
 
